bdd5e676fba83a6c27f4d11946e12b78a1b5ccad
Valentyna2009 <valiaganzha@gmail.com>
2026-06-16 22:39:07 +0200

    adding birdview function for roads

--- main.py
+++ main.py
@@ -35,16 +35,43 @@ def find_lane_lines(img):
     #blur the images
     img_gauss = cv2.GaussianBlur(gray, (11,11), 0)
 
+    # draw the lines (dont know what does the numbers mean)
     thresh_low = 150
     tresh_high = 200
     img_canny = cv2.Canny(img_gauss, thresh_low, tresh_high)
 
     return img_canny
 
+# list with new converted images
 list_img_lines = []
 
+#add modified images to the list and display them
 for img in roads:
     img_lines = find_lane_lines(img)
     list_img_lines.append(img_lines)
 
 show_images(list_img_lines)
+
+
+# vehicle control using a land detection function
+
+# birdview function
+
+def birdview_transform(img):
+    image_height = img.shape[0] 
+    image_width = img.shape[1]
+    
+    src = np.float32([[0, image_height], [image_height, image_height], [0, image_height//3], [image_width, image_height//3]])
+
+    dst = np.float32([[90, image_height], [230, image_height], [-10, 0], [image_width + 10, 0]])
+
+    #transformation matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+
+    warped_img = cv2.warpPerspective(img, M, (image_width, image_height))
+
+    return warped_img
+
+birdview_images = [birdview_transform(img) for img in list_img_lines]
+
+show_images(birdview_images, cmap='gray')
